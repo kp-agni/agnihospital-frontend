2735fc2fa93ddac4eed5501643e3ee457f606270
Kp Agni <kpfreelance12@gmail.com>
2025-04-13 19:07:05 +0530

    Update video URLs to use Google Drive embed format

---
+++ src/config/videoUrls.js
@@ -0,0 +1,13 @@
+const videoUrls = {
+  gallery: {
+    agniAayurved: "https://drive.google.com/uc?export=view&id=1oFM5ncH-HrxfZZcFUfYFH6ptCa-o6OOX",
+    mukhLepan: "https://drive.google.com/uc?export=view&id=1gPF2_STnkdgo7tVgBK1MPE3Az6VANxr3",
+    shirodhara: "https://drive.google.com/uc?export=view&id=1W47_WrFD7CZgz1prdKoa0mvSNsQyyZJG"
+  },
+  testimonials: {
+    testimonial3: "https://drive.google.com/uc?export=view&id=1vL_Eu7uMlc-fp1nXEhOQ4ooVUCTJxqEV",
+    testimonial4: "https://drive.google.com/uc?export=view&id=1rhbwNGhwobtnts3W0O7Vc4cFWUg3a18m"
+  }
+};
+
+export default videoUrls; 
